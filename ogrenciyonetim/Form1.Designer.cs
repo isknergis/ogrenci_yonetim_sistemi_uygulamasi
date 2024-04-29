2e9e15086e6d253f8d0995d55cbd830d467d58e2
isknergis <isknrgs@gmail.com>
2024-04-29 23:29:51 +0300

    Öğrenci Yönetim Bilgi Sistemi
    SQL Veri Tabanı da Kullanılarak C# Windows Form Masaüstü Uygulaması

---
+++ ogrenciyonetim/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace ogrencikayitsistemi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.listBoxOgrenciler = new System.Windows.Forms.ListBox();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.dateTimePickerDogumTarihi = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textbox_ogrencino = new System.Windows.Forms.TextBox();
+            this.textbox_bolum = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btnKaydet.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnKaydet.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKaydet.Location = new System.Drawing.Point(1387, 622);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(241, 40);
+            this.btnKaydet.TabIndex = 0;
+            this.btnKaydet.Text = "Öğrenci Ekle";
+            this.btnKaydet.UseVisualStyleBackColor = false;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // listBoxOgrenciler
+            // 
+            this.listBoxOgrenciler.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(228)))), ((int)(((byte)(216)))), ((int)(((byte)(224)))));
+            this.listBoxOgrenciler.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listBoxOgrenciler.FormattingEnabled = true;
+            this.listBoxOgrenciler.ItemHeight = 20;
+            this.listBoxOgrenciler.Location = new System.Drawing.Point(12, 114);
+            this.listBoxOgrenciler.Name = "listBoxOgrenciler";
+            this.listBoxOgrenciler.Size = new System.Drawing.Size(1244, 464);
+            this.listBoxOgrenciler.TabIndex = 1;
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(1462, 132);
+            this.txtAd.Multiline = true;
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(299, 29);
+            this.txtAd.TabIndex = 2;
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Location = new System.Drawing.Point(1463, 210);
+            this.txtSoyad.Multiline = true;
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(299, 29);
+            this.txtSoyad.TabIndex = 3;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(1463, 299);
+            this.txtEmail.Multiline = true;
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(299, 29);
+            this.txtEmail.TabIndex = 4;
+            // 
+            // dateTimePickerDogumTarihi
+            // 
+            this.dateTimePickerDogumTarihi.Location = new System.Drawing.Point(1540, 535);
+            this.dateTimePickerDogumTarihi.Name = "dateTimePickerDogumTarihi";
+            this.dateTimePickerDogumTarihi.Size = new System.Drawing.Size(221, 22);
+            this.dateTimePickerDogumTarihi.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Silver;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label1.Location = new System.Drawing.Point(1282, 132);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 29);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Öğrenci Adı:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Silver;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label2.Location = new System.Drawing.Point(1282, 210);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(170, 29);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Öğrenci Soyadı:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Silver;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label3.Location = new System.Drawing.Point(1282, 299);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(155, 29);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Öğrenci Email:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Silver;
+            this.label4.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label4.Location = new System.Drawing.Point(1282, 535);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(235, 29);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Öğrenci Doğum Tarihi:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Comic Sans MS", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.SystemColors.Control;
+            this.label5.Location = new System.Drawing.Point(1474, 694);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(288, 32);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Nergis IŞIK 222503033";
+            // 
+            // textbox_ogrencino
+            // 
+            this.textbox_ogrencino.Location = new System.Drawing.Point(1462, 460);
+            this.textbox_ogrencino.Multiline = true;
+            this.textbox_ogrencino.Name = "textbox_ogrencino";
+            this.textbox_ogrencino.Size = new System.Drawing.Size(299, 29);
+            this.textbox_ogrencino.TabIndex = 11;
+            // 
+            // textbox_bolum
+            // 
+            this.textbox_bolum.Location = new System.Drawing.Point(1463, 384);
+            this.textbox_bolum.Multiline = true;
+            this.textbox_bolum.Name = "textbox_bolum";
+            this.textbox_bolum.Size = new System.Drawing.Size(298, 29);
+            this.textbox_bolum.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Silver;
+            this.label6.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label6.Location = new System.Drawing.Point(1282, 384);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(170, 29);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Öğrenci Bölümü:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Silver;
+            this.label7.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label7.Location = new System.Drawing.Point(1282, 460);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(132, 29);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Öğrenci No:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label8.Font = new System.Drawing.Font("Monotype Corsiva", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label8.Location = new System.Drawing.Point(629, 36);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(354, 49);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Öğrenci Kayıt Sistemi";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(1802, 767);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textbox_bolum);
+            this.Controls.Add(this.textbox_ogrencino);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePickerDogumTarihi);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtSoyad);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.listBoxOgrenciler);
+            this.Controls.Add(this.btnKaydet);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.ListBox listBoxOgrenciler;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.DateTimePicker dateTimePickerDogumTarihi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textbox_ogrencino;
+        private System.Windows.Forms.TextBox textbox_bolum;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ ogrenciyonetim/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+
+namespace ogrencikayitsistemi
+{
+    public partial class Form1 : Form
+    {
+
+        private NpgsqlConnection conn;
+
+        public Form1()
+        {
+            InitializeComponent();
+            conn = new NpgsqlConnection("Host = localhost; Port = 5432; Username = postgres; Password = nergis; Database = kutuphane; ");
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+                RefreshOgrenciListesi();
+            
+
+        }
+
+        private void RefreshOgrenciListesi()
+        { //Form her yüklendiğinde öğrenci listesinin güncellenmesini sağlıyor
+
+            try
+            {
+                conn.Open();
+                string query = "SELECT ad, soyad, email, kayit_tarihi, dogum_tarihi, bolum , ogrenci_no FROM ogrenciler";
+                NpgsqlCommand command = new NpgsqlCommand(query, conn);
+                NpgsqlDataAdapter adapter = new NpgsqlDataAdapter(command);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+
+                listBoxOgrenciler.Items.Clear();
+
+                foreach (DataRow row in dataTable.Rows)
+                {
+                    // Her bir satırı ListBox'a ekliyoruz
+                    string ogrenciBilgisi = $"{row["ad"]} {row["soyad"]} - {row["email"]} -" +
+                        $" Kayıt Tarihi: {((DateTime)row["kayit_tarihi"]).ToString("yyyy-MM-dd")} -" +
+                        $" Doğum Tarihi: {((DateTime)row["dogum_tarihi"]).ToString("yyyy-MM-dd")}  - " +
+                        $"Bölümü: {row["bolum"]} - Öğrenci No: {row["ogrenci_no"]}";
+
+                    listBoxOgrenciler.Items.Add(ogrenciBilgisi);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            
+            string ad = txtAd.Text;
+            string soyad = txtSoyad.Text;
+            string email = txtEmail.Text;
+            DateTime dogumTarihi = dateTimePickerDogumTarihi.Value;
+            string bolum = textbox_bolum.Text;
+            string ogrenci_no =textbox_ogrencino.Text;
+
+
+            try
+            {
+                conn.Open();
+                string query = "INSERT INTO ogrenciler (ad, soyad, email, dogum_tarihi, bolum , ogrenci_no) VALUES (@ad, @soyad, @email, @dogum_tarihi , @bolum , @ogrenci_no)";
+                NpgsqlCommand command = new NpgsqlCommand(query, conn);
+                command.Parameters.AddWithValue("@ad", ad);
+                command.Parameters.AddWithValue("@soyad", soyad);
+                command.Parameters.AddWithValue("@email", email);
+                command.Parameters.AddWithValue("@dogum_tarihi", dogumTarihi);
+                command.Parameters.AddWithValue("@bolum", bolum);
+                command.Parameters.AddWithValue("@ogrenci_no", ogrenci_no);
+
+                int rowsAffected = command.ExecuteNonQuery(); // sorguyu çalıştır, etkilenen satır sayısını alır.
+                if (rowsAffected > 0)
+                {
+                    MessageBox.Show("Öğrenci başarıyla kaydedildi.");
+                    RefreshOgrenciListesi();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
+    }
+
